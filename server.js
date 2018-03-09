0b97fdf9dfd3eb099547afa310c211314a55aa6c
Loan Le <32397554+leloan04@users.noreply.github.com>
2018-03-09 15:19:50 -0500

    server

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+var app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static("public"));
